7c68e23abca1f0ef49ca8299d7333b5b3dcd8eef
Gregory Hazen <ghazen@cscc.edu>
2024-01-17 18:17:41 -0500

    Adding Location header

--- src/main/java/com/spankinfresh/blog/api/BlogPostController.java
+++ src/main/java/com/spankinfresh/blog/api/BlogPostController.java
@@ -1,5 +1,6 @@
 package com.spankinfresh.blog.api;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class BlogPostController {
     @PostMapping
     public ResponseEntity createBlogEntry() {
-        return new ResponseEntity(HttpStatus.CREATED);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "http://localhost/api/articles/1");
+
+        return new ResponseEntity(headers, HttpStatus.CREATED);
     }
 }
--- src/test/java/com/spankinfresh/blog/api/BlogPostControllerTests.java
+++ src/test/java/com/spankinfresh/blog/api/BlogPostControllerTests.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -17,5 +19,13 @@ public class BlogPostControllerTests {
     @DisplayName("Post returns status code CREATED")
     public void test01(@Autowired MockMvc mockMvc) throws Exception {
         mockMvc.perform(post("/api/articles")).andExpect(status().isCreated());
+    }
+
+    @Test
+    @DisplayName("Post returns Location header")
+    public void test02(@Autowired MockMvc mockMvc) throws Exception {
+        MvcResult result = mockMvc.perform(post("/api/articles")).andReturn();
+
+        assertEquals("http://localhost/api/articles/1", result.getResponse().getHeader("Location"));
     }
 }
