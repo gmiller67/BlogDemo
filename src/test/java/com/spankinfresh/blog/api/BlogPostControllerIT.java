8bebda1cc1ec0bff4d2646601d70983087cc455a
Gregory Hazen <ghazen@cscc.edu>
2024-01-24 18:05:09 -0500

    Removed mock from IT

--- src/test/java/com/spankinfresh/blog/api/BlogPostControllerIT.java
+++ src/test/java/com/spankinfresh/blog/api/BlogPostControllerIT.java
@@ -1,12 +1,10 @@
 package com.spankinfresh.blog.api;
 
-import com.spankinfresh.blog.data.BlogPostRepository;
 import com.spankinfresh.blog.domain.BlogPost;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
@@ -16,8 +14,6 @@ import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class BlogPostControllerIT {
@@ -30,8 +26,6 @@ public class BlogPostControllerIT {
             new BlogPost(0L, "category", null, "title", "content");
     private static final BlogPost savedPosting =
             new BlogPost(1L, "category", LocalDateTime.now(), "title", "content");
-    @MockBean
-    private BlogPostRepository mockRepository;
 
     @Test
     @DisplayName("POST Location includes server port")
@@ -45,7 +39,6 @@ public class BlogPostControllerIT {
     @Test
     @DisplayName("POST generates nonzero ID")
     public void test02() {
-        when(mockRepository.save(any())).thenReturn(savedPosting);
         ResponseEntity<BlogPost> responseEntity =
                 this.restTemplate.postForEntity(String.format(RESOURCE_URI, localServerPort), testPosting, BlogPost.class);
         assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
