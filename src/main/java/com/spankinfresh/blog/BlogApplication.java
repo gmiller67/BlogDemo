98e29fd8b2e7c862618f7c26a5211edb0ed61973
Gregory Hazen <ghazen@cscc.edu>
2024-02-07 18:00:05 -0500

    Added CORS headers

--- src/main/java/com/spankinfresh/blog/BlogApplication.java
+++ src/main/java/com/spankinfresh/blog/BlogApplication.java
@@ -2,6 +2,14 @@ package com.spankinfresh.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Collections;
 
 @SpringBootApplication
 public class BlogApplication {
@@ -9,5 +17,22 @@ public class BlogApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
 	}
+
+	@Bean
+	public FilterRegistrationBean<CorsFilter> corsConfigFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.setAllowedOrigins(Collections.singletonList("http://localhost:8080"));
+		config.setAllowedMethods(Collections.singletonList("*"));
+		config.setAllowedHeaders(Collections.singletonList("*"));
+		config.addExposedHeader("Location");
+		source.registerCorsConfiguration("/api/**", config);
+		FilterRegistrationBean<CorsFilter> bean =
+				new FilterRegistrationBean<>(new CorsFilter(source));
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return bean;
+	}
+
 
 }
